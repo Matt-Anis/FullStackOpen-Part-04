a2d402ebd79b88b62aee99856be08ca21912c8ee
Matt Anis <webdev.omis@gmail.com>
2026-05-26 13:17:58 +0100

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,18 @@ const totalLikes = blogs => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    return blogs.reduce((favorite, blog) => {
+        return blog.likes > favorite.likes ? blog : favorite
+    }, blogs[0])
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
