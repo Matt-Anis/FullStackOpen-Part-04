4f2e27a134ec01c26169b16751e8669e8a395b6d
Matt Anis <webdev.omis@gmail.com>
2026-06-11 01:26:43 +0100

    4.17: Blog List Expansion, step 5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,11 @@ const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    passwordHash: 1,
+  });
   response.json(blogs);
 });
 
